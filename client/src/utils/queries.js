69c1f66282f9008e2a0b1848c378582cb310fc44
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-16 17:58:34 -0500

    add me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,20 @@
 import gql from 'graphql-tag';
 
+export const QUERY_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
