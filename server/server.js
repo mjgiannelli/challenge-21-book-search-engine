a32ca405da119ef6d9611a6671a5e25a71f7fe01
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-18 12:04:19 -0500

    add morgan npm package

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+var morgan = require('morgan')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,6 +19,7 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('tiny'))
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
